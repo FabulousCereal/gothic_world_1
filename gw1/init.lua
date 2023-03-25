dd9fb06ca575de613eaf1b3ffaf13a795cf609eb
FabulousCereal <none>
2023-03-25 17:10:05 -0300

    Instrucciones de uso

--- gw1/init.lua
+++ gw1/init.lua
@@ -1,49 +1,31 @@
 -- SPDX-FileCopyrightText: 2023 Grupo Warominutes
 -- SPDX-License-Identifier: Unlicense
 
-local function snowParticles(screenW)
+local function snowParticles(screenW, screenH)
 	local graphics = love.graphics
 
-	local size = 2
+	local size = 3
 	local cnv = graphics.newCanvas(size*2, size*2)
 	graphics.setCanvas(cnv)
 	graphics.draw(f0b.elem.circle, size, size, 0, size)
 	graphics.setCanvas()
 
+	local speed = 12
+	local span = screenH + size*2
 	local snow = graphics.newParticleSystem(cnv, 512)
 	snow:setDirection(math.pi/2)
 	snow:setSpeed(12)
 	snow:setPosition(screenW, -size)
 	snow:setEmissionArea("uniform", screenW, size)
 	snow:setEmissionRate(6)
-	snow:setParticleLifetime(60)
-	snow:setSizes(4/3)
+	snow:setParticleLifetime(span / 12)
+	snow:setSizes(1)
 	snow:start()
 	return snow
 end
 
-local function creditScreen(style, w, h)
-	local c, dc = style.color, style.disabled.color
-	local credit = {dc, "Basada en una historia original\npor el ",
-		c, "G", dc, "rupo\n\t\t\t\t",
-		c, "W", dc, "arominutes.\n\nGráficos adicionales por ",
-		c, "Z", dc, "eh ", c, "R", dc, "oh."}
-	local text = love.graphics.newText(style.font, credit)
-	local textW, textH = text:getDimensions()
-	return {text,
-		math.floor(w / 2 - textW / 2),
-		math.floor(h / 2 - textH / 2)}
-end
-
-local function menuScreen(style, w, h)
-	local text = love.graphics.newText(style.font, "Gothic World 1")
-	local textW, textH = text:getDimensions()
-	return {text,
-		math.floor(w * 2/3 - textW / 2),
-		math.floor(h * 2/3 - textH / 2)}
-end
-
 local fload = f0b.std.dofile
+local screen = fload("res/data/screen.lua")
 
 local vn = fload("engine/vn.lua")
 local toc = fload("engine/toc.lua")
@@ -54,16 +36,20 @@ local elemColor = {0, 0, 0, 1/3}
 
 local vnID = gamestate:add(vn.new(res.index, res.style.vn))
 local tocID = gamestate:add(toc.new(res.index, res.style.menu, vnID))
-local credID = gamestate:add(stub.new(), credBG)
+local credID = gamestate:add(stub.new())
+local helpID = gamestate:add(stub.new())
 gamestate[tocID].tracks = f0b.jukebox.newTracklist(
 	{nil, 1, true, false, source="Pronóstico de Nieve (calliope ver).ogg"}
 )
 gamestate[tocID].background = f0b.layers.normalize({
 	{args={"menu/index.png"}, color={1, 1, 1, 1}, distance=4},
-	{args={snowParticles(w)}, distance=1},
+	{args={snowParticles(w, h)}, distance=1},
 })
 gamestate[credID].background = {
-	{args=creditScreen(res.style.menu, w, h)}
+	{args=screen.credit(res.style.menu, w, h)}
+}
+gamestate[helpID].background = {
+	{args=screen.help(res.style.menu, w, h)}
 }
 
 local confBG = f0b.layers.normalize({
@@ -88,6 +74,7 @@ local mainEntries = {
 		{"Volver", "return"},
 		x = -9, y = -9, background = confBG,
 	}},
+	{"Ayuda", "state", helpID},
 	{"Créditos", "state", credID},
 	{"Salir", "state", true},
 	x = 3, y = 3, background = mainBG,
@@ -95,5 +82,5 @@ local mainEntries = {
 
 local mainMenu = menu.new(mainEntries, res.style.menu)
 mainMenu.tracks = f0b.jukebox.newTracklist({source="Viento.ogg"})
-mainMenu.backgroundColor = {0,0,0,1} --{1,1,1,1}
+mainMenu.backgroundColor = {0,0,0,1}
 gamestate:add(mainMenu, nil, true)
---
+++ gw1/res/data/screen.lua
@@ -0,0 +1,84 @@
+-- SPDX-FileCopyrightText: 2023 Grupo Warominutes
+-- SPDX-License-Identifier: Unlicense
+
+local function center(text, w, h, textW, textH)
+	return {text,
+		math.floor(w / 2 - textW / 2),
+		math.floor(h / 2 - textH / 2)}
+end
+
+return {
+	help = function(style, w, h)
+		local c = style.color
+		local dc = style.disabled.color
+		local font = res.fonts("takaoMincho", 30)
+		local em = font:getWidth(" ")
+		local sep = ""
+
+		local help = {
+			{"Controles",
+				{"Seleccionar, avanzar", "Enter"},
+				{"Avanzar texto", "Espacio"},
+				{"Mover selección", "←", "↑", "→", "↓"},
+				{"Sección previa/siguiente", "RePág", "AvPág"},
+				{"Ir al inicio/final", "Home", "End"},
+				{"Volver al menú previo", "←  "},
+				{"Escapar", "ESC"},
+			},
+			{"Secretos",
+				{"Disminuir rojo/verde/azul", "r", "g", "b"},
+				{"Aumentar rojo/verde/azul", "R", "G", "B"},
+			},
+		}
+
+		local text = love.graphics.newText(font)
+		local y = 0
+		local maxX = 0
+		local ins = table.insert
+		for i = 1, #help do
+			local section = help[i]
+			local desc = {c, "\n"}
+			local keys = {c, section[1] .. "\n"}
+			for k = 2, #section do
+				local line = section[k]
+				ins(desc, c)
+				ins(desc, line[1] .. "\n")
+				for m = 2, #line do
+					ins(keys, dc)
+					ins(keys, "[")
+					ins(keys, c)
+					ins(keys, line[m])
+					ins(keys, dc)
+					ins(keys, "]")
+				end
+				ins(keys, c)
+				ins(keys, sep .. "\n")
+			end
+			text:addf(keys, w, "left", 0, y)
+			local textW, textH = text:getDimensions()
+			textW = textW + em
+			text:addf(desc, w, "left", textW, y)
+			y = y + textH
+			maxX = math.max(maxX, textW + text:getWidth())
+		end
+		return center(text, w, h, maxX, y)
+	end,
+
+	credit = function(style, w, h)
+		local c, dc = style.color, style.disabled.color
+		local credit = {dc, "Basada en una historia original\npor el ",
+			c, "G", dc, "rupo\n\t\t\t\t",
+			c, "W", dc, "arominutes.\n\nGráficos adicionales por ",
+			c, "Z", dc, "eh ", c, "R", dc, "oh."}
+		local text = love.graphics.newText(style.font, credit)
+		return center(text, w, h, text:getDimensions())
+	end,
+
+	menu = function(style, w, h)
+		local text = love.graphics.newText(style.font, "Gothic World 1")
+		local textW, textH = text:getDimensions()
+		return {text,
+			math.floor(w * 2/3 - textW / 2),
+			math.floor(h * 2/3 - textH / 2)}
+	end,
+}

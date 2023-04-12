577f03327d1985478909de9cf94b0940443736f0
FabulousCereal <none>
2023-04-11 21:12:28 -0400

    Continuación del relato de Croft

--- gw1/res/data/05.croft_days/22.lua
+++ gw1/res/data/05.croft_days/22.lua
@@ -1,10 +1,127 @@
 return {
 	{"style", "vnCroft"},
-	{"name", "Croft"},
-	[[No ví a ninguno de mis vecinos durante aquel día, y para las 8
-seguiamos sin electricidad. Tampoco nadie contestaba mis llamadas, así que dejé
-de intentar para no agotar la batería del celular. Con la completa oscuridad de
-la noche nos fuimos a acostar con Rocco. Mañana veriamos que hacer.]],
+	res.fun.macro.title,
+	{"macro", res.fun.macro.date, {2011, 11, 9, 17, 12}},
 
-	[[(aquí pasa algo con una barricada pero lo escribiré después)]]
+	{"name", "Croft"},
+	[[Seguía en casa para la tarde. Habiamos comido un bocadillo con Rocco,
+y me encontraba reparando unas goteras que tenía hace años en el techo.]],
+
+	[[Claro, cuando llovía, no lo hacía para no mojarme, y cuando no, no
+había para que molestarse. Pero sin electricidad ni internet, es increíble lo
+que rinde el tiempo.]],
+
+	[[Y además, así mantenía la mente ocupada. Mi amigo estaba muerto.]],
+
+	[[Rocco se puso a ladrarle a la calle, y vi que alguien se acercaba, la
+primera persona que veía por estos lares. Era mi vecino Bill, que venía con su
+boina y ritmo relajado de siempre.]],
+
+	[[Me apresuré en bajar a recibirlo. Bill se metió al patio, y Rocco
+corrió a mis pies.]],
+
+	[["¡Bill! ¡Que bueno que veo a alguien! ¡He tenido un día de locos!"]],
+
+	{"name", "Bill"},
+	[["..."]],
+
+	{"name", "Croft"},
+	[["¿Sabes que está pasando?"]],
+
+	{"name", "Bill"},
+	[["..."]],
+
+	{"name", "Croft"},
+	[["¿Bill?" Bill siempre andaba algo encorvado, pero hoy apenas y le
+veía los ojos. Me acerqué a el. Mala decisión.]],
+
+	{"name", "Bill"},
+	[["B... Bwaa..."]],
+
+	{"name", "Croft"},
+	[["¿Bua que?"]],
+
+	{"name", "Bill"},
+	[["¡¡Bwaaa!!"]],
+
+	{"name", "Croft"},
+	[[Bill se abalanzó sobre mí, y por poco logré esquivarlo. Entendí
+entonces, también había sido afectado.]],
+
+	[[Corrimos con Rocco de vuelta a la casa y cerré la puerta antes que
+nos alcanzara, pero Bill se estrelló contra ella y estremeció toda la casa,
+como si nisiquiera hubiera intentado frenar. Y luego siguió golpeando.]],
+
+	[["¡Bill! ¡Bill!" Intenté hablarle durante dos minutos, pero fue en
+vano. Cuando ví que iba a romper la puerta, supe que iba a tener que huir o
+detenerlo. Había un cuchillo a mi alcance.]],
+
+	[[Decidí huir. Era mi vecino. No podía lastimarlo.]],
+
+	[[Escapé por atrás al tiempo que Bill hechaba la puerta abajo.
+Aproveché la ventaja de estar afuera, y corrí a toda velocidad hasta la
+camioneta. Me subí a darle contacto.]],
+
+	[[Mi Vieja Mula pareció leer mi mente, porque encendió de inmediato.
+Rocco se subió de un salto y nos fuimos antes que Bill volviera.]],
+
+	[[Salí al camino, y en un impulso me fuí en dirección contraria a la
+ciudad.]],
+
+	[[Pasé tras unos minutos frente a la casa de Bill. Tenía la puerta
+abierta, con manchas de sangre adentro.]],
+
+	[[Solo podían ser de Isidora, su esposa, pero no tenía ganas de
+averiguarlo. Aceleré, iba a irme a la frontera.]],
+
+	[[(montaje de viaje)]],
+
+	{"macro", res.fun.macro.date, {2011, 11, 9, 19, 01}},
+	[[Estando ya a oscuras, y habiendo usado uno de mis bidones de
+combustible, llegué a una especie de bloqueo militar. Habían unos hombres con
+fusiles resguardando.]],
+
+	[["Ah, por fin." Estacioné el auto y me quité el cinturon para bajarme,
+cuando noté que uno de los hombres se acercaba a mi ventana.]],
+
+	{"name", false},
+	[["Autorización," me pidió, sin mas rodeos.]],
+
+	{"name", "Croft"},
+	[["¿Que?" pregunté sin entender. Con eso quedó claro que no tenía tal
+cosa.]],
+
+	{"name", false},
+	[["Retirese del lugar," me ordernó.]],
+
+	{"name", "Croft"},
+	[["¿Tienen idea de lo que está pasando allá atrás? ¡Necesito refugio!"]],
+
+	[[Fue inútil, el soldado se negaba a razonar. El resto tampoco se veía
+muy amigable. Para empeorar las cosas, Rocco se puso justo a dar vueltas en
+el asiento.]],
+
+	[["¡Espera! ¡Rocco!" Rocco saltó por mi ventana, causando que todos los
+militares le apuntaran, y creo que solo porque aún estaba el otro sujeto al
+lado nuestro no lo llenaron de balas antes de tocar el suelo.]],
+
+	[[Lo dejaron cagar junto a la rueda tranquilo, aunque sin bajar sus
+rifles durante todo el proceso. Apenas Rocco terminó me dieron 10 segundos para
+esfumarme.]],
+
+	[[Traté de ver que había mas allá de la barricada, pero en la oscuridad
+no distinguía nada mas allá de los árboles. También estaba casi seguro de que
+aquí no habían bases militares ni infraestructura crítica.]],
+
+	[[A los 5 segundos decidí no tentar mas mi suerte.]],
+
+	[["Son una vergüenza," les dije mientras daba la vuelta. Iba a tener
+que hacer el largo viaje de regreso.]],
+
+	[[Una vez detrás de una curva evalué mis opciones. Podía ir a la
+ciudad, salir por la autopista, pero algo me decía que me iba a encontrar con
+lo mismo.]],
+
+	[[Mis únicas alternativas parecían ser irme a casa y rogar que Bill se
+haya ido, buscar donde dormir en la ciudad, o dormir en el carro.]],
 }
